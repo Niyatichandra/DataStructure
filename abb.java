73a272b552422830a0cf2ca9ab1c6d60f126e0fe
Niyatichandra <abbca23220_niyati@banasthali.in>
2024-06-24 12:26:52 +0530

    push

---
+++ abb.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Scanner;
+public class abb {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        abbr(s, "", 0, 0);
+
+    }
+
+    public static void abbr(String s, String asf, int count, int pos) {
+        if (pos == s.length()) {
+            if (count == 0) {
+                System.out.println(asf);
+            } else {
+                System.out.println(asf + count);
+            }
+            return;
+        }
+
+        if (count > 0)
+            abbr(s, asf + count + s.charAt(pos), 0, pos + 1);
+        else
+            abbr(s, asf + s.charAt(pos), 0, pos + 1);
+
+        abbr(s, asf, count + 1, pos + 1);
+    }
+}
