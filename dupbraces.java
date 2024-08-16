23e3c0aa031d2e3a242463d7fb5677a94ebe09dc
Niyatichandra <abbca23220_niyati@banasthali.in>
2024-08-17 00:23:57 +0530

    push

---
+++ dupbraces.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+import java.util.Scanner;
+public class dupbraces {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        Stack <Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch==')') {
+                if (st.peek() == '(') {
+                    System.out.println(true);
+                    return;
+                } else {
+                    while (st.peek() != '(') {
+                        st.pop();
+                    }
+                    st.pop();
+                }
+            }
+            else
+
+                {
+                    st.push(ch);
+                }
+            }
+        }
+    }
+
